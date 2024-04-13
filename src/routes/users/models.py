4141acd17084a9638a722132506852adeb57ded0
Brajenko <goshabrajenko@gmail.com>
2024-04-13 19:52:52 +0300

    rewrite models to mapped

--- src/routes/users/models.py
+++ src/routes/users/models.py
@@ -1,14 +1,21 @@
+from typing import Optional
+
 from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.config.database import Base
+from src.routes.articles.models import Article, favorites_to_users_table
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(length=100), unique=True, nullable=False)
-    username = Column(String(length=50), unique=True, nullable=False)
-    bio = Column(Text(), nullable=True)
-    image = Column(String(length=255), nullable=True, default='https://api.realworld.io/images/smiley-cyrus.jpeg')
-    password = Column(String(length=255))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(length=100), unique=True)
+    username: Mapped[str] = mapped_column(String(length=50), unique=True)
+    bio: Mapped[Optional[str]] = mapped_column(Text())
+    image: Mapped[str] = mapped_column(default='https://api.realworld.io/images/smiley-cyrus.jpeg')
+    password: Mapped[str]
+    favorited_articles: Mapped[list[Article]] = relationship(
+        secondary=favorites_to_users_table, back_populates='users'
+    )
